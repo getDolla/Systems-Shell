5fc59b7d77d027c90b8cd7eb89830e7820342748
Brian Yang <brian.yang328@gmail.com>
2016-11-23 19:54:54 -0500

    Fix cd again

--- cd.c
+++ cd.c
@@ -4,12 +4,7 @@
 #include <string.h>
 #include <errno.h>
 
-int cd(char* dir) {
-  chdir(dir);
-  if (errno) {
-    printf("Error chdir %d: %s\n", errno, strerror(errno));
-  }
-
+void print_working_dir() {
   char cwd[1024];
   if (getcwd(cwd, sizeof(cwd)) != NULL) {
     printf("Current working dir: %s\n", cwd);
@@ -17,12 +12,40 @@ int cd(char* dir) {
   if (errno) {
     printf("Error cwd %d: %s\n", errno, strerror(errno));
   }
+}
+
+void chdir_with_tilde(char* dir) {
+  char home[1024];
+  strcpy(home, getenv("HOME"));
+  chdir(strcat(getenv("HOME"), dir + 1));
+  if (errno) {
+    printf("Error chdir with ~ (tilde) %d: %s\n", errno, strerror(errno));
+  }
+  strcpy(getenv("HOME"), home);
+}
+
+int cd(char* dir) {
+  if (*dir == '~') {
+    chdir_with_tilde(dir);
+  } else {
+    chdir(dir);
+  }
+  if (errno) {
+    printf("Error chdir %d: %s\n", errno, strerror(errno));
+  }
+
+  print_working_dir();
 
   return 0;
 }
 
-int main() {
-  /* cd("/home"); */
-  cd("~");
-  return 0;
-}
+/* int main() { */
+/*   cd(".."); */
+/*   cd("~/Desktop"); */
+/*   cd("/"); */
+/*   cd(".."); */
+/*   cd("~/Desktop"); */
+/*   cd("../Downloads"); */
+/*   cd("~"); */
+/*   return 0; */
+/* } */
--- cd.h
+++ cd.h
@@ -1,6 +1,8 @@
-/* 
+/*
    @params: takes a char* dir which is the name of the directory to cd into
    @return: returns nothing
    @purpose: implements the cd terminal command for our custom shell
  */
-void cd(char * dir);
+void cd(char* dir);
+void chdir_with_tilde(char* dir);
+void print_working_dir();
