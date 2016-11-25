73a23c4ecb47135c4dcf119f1083ebb3e4e75ae8
Brian Yang <brian.yang328@gmail.com>
2016-11-25 15:07:46 -0500

    Fix subprocess error

--- shell.c
+++ shell.c
@@ -15,56 +15,60 @@ void pwd(char* cur_dir) {
   printf("%s$ ", cur_dir);
 }
 
-void execute_command(char* command_ptr, char* args) {
+void print_exit_status(int status) {
+  if (WIFEXITED(status)) {
+    printf("Exit status: %d\n", WEXITSTATUS(status));
+    if (WEXITSTATUS(status)) {
+      printf("Error: %s\n", strerror(WEXITSTATUS(status)));
+    }
+  }
 }
 
 int main(int argc, char *argv[]){
-  printf("Press ctrl-c to stop the program. Exit not yet implemented\n");
-  // stores current directory
-  char cur_dir[1024];
+  printf("Press ctrl-c to exit.");
+  char cur_dir[1024]; // stores current directory
 
-  // for command
-  char command[1024];
+  char command[1024]; // command buffer
 
-  // for strsep
-  char *command_ptr = command;
-  char *args[10];
+  char *command_ptr = command; // for parsing by strsep
+  char *args[10]; // stores arguments for execvp
 
-  // for subprocess
-  int status;
-  pid_t pid = 1;
+  int status; // stores status of the child process
+  pid_t pid; // stores the pid returned by fork
 
   // make shell persistent
   while (1) {
     pwd(cur_dir);
 
     fgets(command, sizeof(command), stdin); // gets input from stream
-    if ( !(strncmp(command, "exit", 4) == 0 || strncmp(command, "cd", 2) == 0) ) {
+
+    if ( strcmp(command, "exit") != 0 && strncmp(command, "cd ", 3) != 0 ) {
       pid = fork();
-      wait(&status);
-    }
 
-    if (pid == 0) {
-      if (command[strlen(command) - 1] == '\n') {
-	command[strlen(command) - 1] = '\0';
+      if (pid > 0) {
+	wait(&status);
+	print_exit_status(status);
+      } else if (pid == 0) {
+	if (command[strlen(command) - 1] == '\n') {
+	  command[strlen(command) - 1] = '\0';
+	}
+
+	int i = 0;
+	while (command_ptr) {
+	  args[i] = strsep(&command_ptr, " "); // parses series of arguments
+	  i++;
+	}
+	args[i] = NULL; // add terminating null - necessary
+
+	execvp(args[0], args);
+
+	exit(errno);
+      } else if (pid == -1) {
+	printf("Subprocess error: %s\n", strerror(errno));
       }
 
-      int i = 0;
-      printf("%s\n", command); //debug
-      while(command_ptr){
-	args[i] = strsep(&command_ptr, " "); // parses series of arguments
-	// printf("%s\n", args[i]);//debug
-	i++;
-      }
-
-      args[i] = NULL; // add terminating null - necessary
-      execvp(args[0], args);
-      if (errno) {
-	printf("Execvp error: %s\n", strerror(errno));
-      }
-    } else if (pid == -1) {
-      printf("Subprocess error: %s\n", strerror(errno));
     }
   }
+
   return 0;
 }
